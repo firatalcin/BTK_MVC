f9a06cecc4cdc5e4dcca7788253dce4411f98483
Fırat Alçın <firatalcin04@gmail.com>
2025-06-01 14:23:01 +0300

    Inversion Of Control

--- Store/Controllers/ProductController.cs
+++ Store/Controllers/ProductController.cs
@@ -1,23 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Store.Models;
 
-namespace Store.Controllers
+namespace Store.Controllers;
+
+public class ProductController : Controller
 {
-    public class ProductController : Controller
+    private readonly RepositoryContext _context;
+
+    public ProductController(RepositoryContext context)
     {
-        private readonly RepositoryContext _context;
+        _context = context;
+    }
 
-        public ProductController(RepositoryContext context)
-        {
-            _context = context;
-        }
-        
-        public IActionResult Index()
-        {
-            var model = _context.Products.ToList();
-            return View(model);
-        }
+    public IActionResult Index()
+    {
+        var model = _context.Products.ToList();
+        return View(model);
+    }
 
+    public IActionResult Get(int id)
+    {
+        var product = _context.Products.Find(id);
+        return View(product);
     }
 }
