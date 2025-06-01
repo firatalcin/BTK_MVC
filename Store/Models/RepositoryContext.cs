532c4cf4cfa1af188bbab0361beb4684a852bb77
Fırat Alçın <firatalcin04@gmail.com>
2025-06-01 11:55:25 +0300

    Paket Yönetimi ve Repository Tanımı

---
+++ Store/Models/RepositoryContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Store.Models;
+
+public class RepositoryContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+}
--- Store/Program.cs
+++ Store/Program.cs
@@ -3,8 +3,7 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/btk", () => "BTK Akademi");
+
 
 app.UseHttpsRedirection();
 app.UseRouting();
