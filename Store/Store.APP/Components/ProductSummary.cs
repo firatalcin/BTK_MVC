46c9a16c24e0a61881a811932faca6d85261aeb7
Fırat Alçın <firatalcin04@gmail.com>
2025-06-23 21:37:57 +0300

    ViewComponent

---
+++ Store/Store.APP/Components/ProductSummary.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.Repositories;
+using Store.Services.Contracts;
+
+namespace Store.APP.Components;
+
+public class ProductSummary : ViewComponent
+{
+    private readonly IServiceManager _serviceManager;
+
+    public ProductSummary(IServiceManager serviceManager)
+    {
+        _serviceManager = serviceManager;
+    }
+
+    public string Invoke()
+    {
+        return _serviceManager.ProductService.GetAllProducts(false).Count().ToString();
+    }
+}
