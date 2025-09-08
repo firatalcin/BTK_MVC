06fecf3a3be15b141ca8b8d1b6be5458e71b518d
Fırat Alçın <firatalcin04@gmail.com>
2025-09-08 22:15:41 +0300

    CartSummary Widget

---
+++ Store/Store.APP/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.Entities.Models;
+
+namespace Store.APP.Components;
+
+public class CartSummaryViewComponent : ViewComponent
+{
+    private readonly Cart _cart;
+
+    public CartSummaryViewComponent(Cart cartService)
+    {
+        _cart  = cartService;
+    }
+
+    public string Invoke()
+    {
+        return _cart.Lines.Count.ToString();
+    }
+}
